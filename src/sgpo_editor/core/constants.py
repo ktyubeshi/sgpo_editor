7bf5cfc20ee5f7d459c7c83c5a409a89c33a02c4
ktyubeshi <ktyubeshi@users.noreply.github.com>
2025-03-15 03:50:02 +0900

    国際化機能の実装: TranslationStatusクラスと翻訳機能を追加

---
+++ src/sgpo_editor/core/constants.py
@@ -0,0 +1,21 @@
+"""定数定義モジュール
+
+このモジュールは、アプリケーション全体で使用される定数を定義します。
+"""
+
+# 翻訳ステータス定数
+class TranslationStatus:
+    """翻訳ステータスを表す定数クラス"""
+    ALL = "all"
+    TRANSLATED = "translated"
+    UNTRANSLATED = "untranslated"
+    FUZZY = "fuzzy"
+    OBSOLETE = "obsolete"
+
+# ステータスの表示順序
+TRANSLATION_STATUS_ORDER = [
+    TranslationStatus.ALL,
+    TranslationStatus.TRANSLATED,
+    TranslationStatus.UNTRANSLATED,
+    TranslationStatus.FUZZY,
+] 
