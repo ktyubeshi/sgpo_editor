ebedd816a463d9e172acc26b26d2b502eaebcc51
yubeshi <kt.yubeshi@gmail.com>
2025-04-30 17:42:03 +0900

    Update pytest configuration and remove windsurf monitor script

--- windsurf_monitor.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash         # ← shebang を bash に
-WATCH="$HOME/.codeium/windsurf/cascade"
-
-fswatch -0 -t --event=Updated "$WATCH" | \
-while IFS= read -r -d '' time path; do
-  printf '%(%F %T)T BUSY  %s\n' "${time%.*}" "$path"
-done
